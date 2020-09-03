a86c912df52de3c3afabe57f39e97ce43cb0cd1f
milos381 <milos_popovic@outlook.com>
2020-09-02 22:35:47 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,8 +2,25 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
-
-    # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+    get '/new' do
+      erb :'pirates/new'
+    end
+    post '/pirates' do
+      #binding.pry
+      
+      #<input type="text" id="name" name="pirate[name] and the others">
+      @pirate = Pirate.new(params[:pirate])
+      
+      #<input type="text" id="ship_name_1" name="pirate[ships][][name] and the others"
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+      @ships = Ship.all
+      erb :'pirates/show'
+    end
 
   end
 end
